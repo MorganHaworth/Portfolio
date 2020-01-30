0722449a8b2bb5401156c73f44827ac2d5eb200d
MorganHaworth <morgan123haworth@gmail.com>
2020-01-29 23:34:29 -0600

    Add styling to cards and navbar. Fix v-for key issues

--- portfolio/src/components/Menu.vue
+++ portfolio/src/components/Menu.vue
@@ -1,49 +1,48 @@
 <template>
     <div>
-        <nav class="navbar navbar-expand-sm navbar-light bg-light">
+        <nav class="navbar navbar-expand-sm navbar-dark shadow-sm myNavBar">
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav mr-auto">
-                <li class="nav-item active">
-                    
-                    <router-link to="/" class="nav-item nav-link active" href="#">Home<span class="sr-only">(current)</span></router-link>
-                </li>
-                <li class="nav-item">
-                    <router-link to="/about" class="nav-item nav-link" href="#">About</router-link>
-                </li>
-                <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                    Dropdown
-                    </a>
-                    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                    <a class="dropdown-item" href="#">Action</a>
-                    <a class="dropdown-item" href="#">Another action</a>
-                    <div class="dropdown-divider"></div>
-                    <a class="dropdown-item" href="#">Something else here</a>
-                    </div>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link disabled" href="#">Disabled</a>
-                </li>
+                    <li class="nav-item">
+                        <router-link to="/" class="nav-item nav-link router-link-active" href="#">Home<span class="sr-only">(current)</span></router-link>
+                    </li>
+                    <li class="nav-item">
+                        <router-link to="/about" class="nav-item nav-link router-link-active" href="#">About</router-link>
+                    </li>
+                    <!--<li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        Dropdown
+                        </a>
+                        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                        <a class="dropdown-item" href="#">Action</a>
+                        <a class="dropdown-item" href="#">Another action</a>
+                        <div class="dropdown-divider"></div>
+                        <a class="dropdown-item" href="#">Something else here</a>
+                        </div>
+                    </li>-->
+                    <!--<li class="nav-item">
+                        <a class="nav-link disabled" href="#">Disabled</a>
+                    </li>-->
                 </ul>
             </div>
             <div class="navbar-collapse collapse w-100 order-3 dual-collapse2" id="navbarSupportedContent">
-                <ul class="navbar-nav ml-auto">
-                    <li class="nav-item">
+                <ul class="navbar-nav ml-auto navBgColor"> <!--  background color of icons -->
+                    <li class="nav-item brandIcon">
                         <a class="nav-link" href="mailto:morgan123haworth@gmail.com">
-                            <font-awesome-icon icon="envelope"/>
+                            <font-awesome-icon size="lg" icon="envelope"/>
                         </a>
                     </li>
-                    <li class="nav-item">
+                    <li class="nav-item brandIcon">
                         <a class="nav-link" href="https://github.com/MorganHaworth">
-                            <font-awesome-icon :icon="['fab', 'github']"/>
+                            <font-awesome-icon size="lg" :icon="['fab', 'github']"/>
                         </a>
                     </li>
-                    <li class="nav-item">
+                    <li class="nav-item brandIcon">
                         <a class="nav-link" href="https://www.linkedin.com/in/morganhaworth/">
-                            <font-awesome-icon :icon="['fab', 'linkedin']"/>
+                            <font-awesome-icon size="lg" :icon="['fab', 'linkedin']"/>
                         </a>
                     </li>
                 </ul>
@@ -51,6 +50,31 @@
         </nav>
     </div>
 </template>
+
+<style scoped>
+.brandIcon {
+    color: white;
+    padding-left: 5px;
+}
+
+.navbar {
+    padding-top: 0;
+    padding-bottom: 0;
+}
+
+nav li {
+    padding-top: 10px;
+    padding-bottom: 10px;
+}
+
+.navBgColor {
+    background-color: #80DDD6;/*#B1EDE8;*/
+}
+
+.myNavBar {
+    background-color: #352D39;
+}
+</style>
 
 <script>
 export default {
--- portfolio/src/views/About.vue
+++ portfolio/src/views/About.vue
@@ -1,42 +1,42 @@
 <template>
     <div class="container">
-        Education
-        <div class="card text-left" v-for="(item, index) in $options.education.education" :key="index">
+        <div class="text-left sectionTitle">Education</div>
+        <div class="card text-left" v-for="(item, index) in $options.education.education" :key="'A'+index">
             <div class="card-body">
                 <h5 class="card-title">{{ item.school }}</h5>
-                <h6 class="card-subtitle text-muted">{{ item.degree }} - GPA: {{ item.GPA }}</h6>
-                <p class="card-subtitle">{{ item.startDate }} - {{ item.graduationDate }}</p>
+                <h6 class="card-subtitle">{{ item.degree }} - GPA: {{ item.GPA }}</h6>
+                <p class="card-subtitle text-muted">{{ item.startDate }} - {{ item.graduationDate }}</p>
                 <ul>
-                    <li v-for="(item, index) in item.responsibilities" :key="index">
+                    <li v-for="(item, index) in item.responsibilities" :key="'B'+index">
                         {{ item }}
                     </li>
                 </ul>
             </div>
         </div>
-        Work Experience
-        <div class="card text-left" v-for="(item, index) in $options.experience.experience.slice().reverse()" :key="index">
+        <div class="text-left sectionTitle">Work Experience</div>
+        <div class="card text-left" v-for="(item, index) in $options.experience.experience.slice().reverse()" :key="'C'+index">
             <div class="card-body">
                 <h5 class="card-title">{{ item.title }}</h5>
-                <h6 class="card-subtitle text-muted">{{ item.company }}</h6>
-                <p class="card-subtitle">{{ item.startDate }} - {{ item.endDate }}</p>
+                <h6 class="card-subtitle">{{ item.company }}</h6>
+                <p class="card-subtitle text-muted">{{ item.startDate }} - {{ item.endDate }}</p>
                 <ul>
-                    <li v-for="(item, index) in item.responsibilities" :key="index">
+                    <li v-for="(item, index) in item.responsibilities" :key="'D'+index">
                         {{ item }}
                     </li>
                 </ul>
             </div>
         </div>
 
-        Activities and Awards
-        <div class="card text-left" v-for="(item, index) in $options.activities.activities" :key="index">
+        <div class="text-left sectionTitle">Activities and Awards</div>
+        <div class="card text-left" v-for="(item, index) in $options.activities.activities" :key="'E'+index">
             <div class="card-body">
                 <h5 class="card-title">{{ item.activity }}</h5>
-                <h6 class="card-subtitle text-muted">
+                <h6 class="card-subtitle">
                     <div>{{ item.project }}<span v-if="item.outcome"> - {{ item.outcome }}</span></div>
                 </h6>
-                <p class="card-subtitle">{{ item.date }}</p>
+                <p class="card-subtitle text-muted">{{ item.date }}</p>
                 <ul>
-                    <li v-for="(item, index) in item.description" :key="index">
+                    <li v-for="(item, index) in item.description" :key="'F'+index">
                         {{ item }}
                     </li>
                 </ul>
@@ -44,6 +44,30 @@
         </div>
     </div>
 </template>
+
+<style scoped>
+.card-title {
+    font-size: 27px;
+    margin-bottom: 12px;
+}
+
+.card-subtitle {
+    margin-top: 0px;
+}
+
+.sectionTitle {
+    font-family: courier, monospace;
+    font-weight: bold;
+    color: #FF6978;
+    text-shadow: 1px 1px rgba(0,0,0,0.3);
+    font-size: 36px;
+    margin-top: 30px;
+}
+
+.card {
+    border: none;
+}
+</style>
 
 <script>
 import activities from '../json/activities.json'
